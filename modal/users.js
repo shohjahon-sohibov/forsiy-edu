e63041e0eb47a3da333a518dc47985f8fbffdca3
shohjahon-sohibov <sohibov.s@bk.ru>
2023-04-18 12:14:05 +0300

    add users.js file to list of users in comment

---
+++ modal/users.js
@@ -0,0 +1,28 @@
+/* 
+Admin
+  name: Umar
+  password: 1
+
+Teachers
+  name: Usmon
+  password: teacher
+
+  name: Ali
+  password: teacher
+
+  name: abdushukur
+  password: teacher
+
+
+Students
+  name: samandar
+  password: student
+
+  name: aziz
+  password: student
+
+  name: ali
+  password: student
+
+  
+*/
